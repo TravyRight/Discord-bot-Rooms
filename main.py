ab55aa6f326a41e1d5a4c2e6058f4b0735d1bd77
travyright (home) <timadubakov@gmail.com>
2025-02-04 11:10:00 +0500

    create default code

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import logging
+
+from config import TOKEN, bot
+
+
 def main():
-    print("test")
+    try:
+        logging.info(f"Bot start in progress...")
+        # logging.debug(f"Database tables were created.")
+
+    except Exception as e:
+        logging.error(f"Failed to start bot. Error: {str(e)}")
+        print(e)
+
+    @bot.event()
+    async def on_ready():
+        print("Bot is ready")
+
+    bot.run(TOKEN)
 
 
 if __name__ == "__main__":
