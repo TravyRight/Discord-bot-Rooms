ace1dda41559a2dba8b3e6926e4ac2c7b563a8d9
travyright (home) <timadubakov@gmail.com>
2025-02-05 17:48:53 +0500

    add locale

--- main.py
+++ main.py
@@ -5,23 +5,18 @@ from database.tables import create_tables
 
 
 def main():
-    try:
-        logging.info(f"Bot start in progress...")
+    logging.info(f"Bot start in progress...")
 
-        create_tables()
-        logging.debug(f"Database tables were created.")
+    create_tables()
+    logging.debug(f"Database tables were created.")
 
-        bot.load_extension("cogs.rooms.commands")
-        bot.load_extension("cogs.rooms.events")
-        logging.debug(f"Cogs were connected.")
+    bot.load_extension("cogs.rooms.commands")
+    bot.load_extension("cogs.rooms.events")
+    logging.debug(f"Cogs were connected.")
 
-        # connect locale
-        bot.i18n.load("locale/")
-        logging.debug(f"locale were loaded.")
-
-    except Exception as e:
-        logging.error(f"Failed to start bot. Error: {str(e)}")
-        print(e)
+    # connect locale
+    bot.i18n.load("Discord-bot-Rooms/locale/")
+    logging.debug(f"locale were loaded.")
 
     @bot.event
     async def on_ready():
